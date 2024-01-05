7e47509c7920a599b58024417050a3e807b2a2ab
jonathan.hendriks <jonathan.j.hendriks@gmail.com>
2024-01-05 09:17:43 +0100

    added indent-blankline-nvim.lua and vim-easy-align.lua

--- user/set.lua
+++ user/set.lua
@@ -24,10 +24,16 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.cursorline = true
 
+
+vim.opt.list = true
+vim.opt.listchars = { space = '.', tab = '|.', eol = '⤶', }
+
 -- Keymaps
 vim.keymap.set("n","<leader>e",vim.cmd.Ex)
 vim.keymap.set("n","<leader>w",":w<CR>")
 vim.keymap.set("n","<leader>q",":q<CR>")
+vim.keymap.set("n","<leader>sw",":w !sudo tee%<CR>")
+vim.keymap.set("n","<leader>so",":so %<CR>")
 
 vim.keymap.set("i","jj","<ESC>")
 
@@ -37,9 +43,21 @@ vim.keymap.set("n","<C-j>","<C-W>j")
 vim.keymap.set("n","<C-k>","<C-W>k")
 vim.keymap.set("n","<C-l>","<C-W>l")
 
+-- Move Window to Left,Right,Top,Bottom with Ctrl+Shift+
+vim.keymap.set("n","<c-J>","<C-W>J")
+vim.keymap.set("n","<c-K>","<C-W>k")
+vim.keymap.set("n","<c-H>","<C-W>H")
+vim.keymap.set("n","<c-L>","<C-W>L")
+
 -- Naviguation Page Up/Down
-vim.keymap.set("n","<C-d>","<C-d>zz")
-vim.keymap.set("n","<C-u>","<C-u>zz")
+vim.keymap.set("n","<C-i>","<C-u>zz")
+vim.keymap.set("n","<C-,>","<C-d>zz")
+
+-- Resize windows
+vim.keymap.set("n","<C-left>",":veritcal resize +5<CR>"); 
+vim.keymap.set("n","<C-right>",":veritcal resize -5<CR>"); 
+vim.keymap.set("n","<C-down>",":resize -5<CR>"); 
+vim.keymap.set("n","<C-up>",":resize +5<CR>"); 
 
 -- Greatest Copy/Paste
 vim.keymap.set("n","<leader>y","\"+y")
