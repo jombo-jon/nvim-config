69893bd9310c0c1d6172686b7d4d999d340b1307
Jonathan Hendriks <jonathan.hendriks@zuehlke.com>
2024-05-02 08:53:10 +0200

    Added floaterm

---
+++ plugin/vim-floaterm.lua
@@ -0,0 +1,8 @@
+-- local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>sn', ":FloatermNew")
+vim.keymap.set('n', '<leader>st>', ":FloatermNew")
+vim.keymap.set('n', '<leader>sb>', ":FloatermNew")
+-- vim.keymap.set('n', '<leader>sb', function()
+-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
