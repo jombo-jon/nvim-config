d00beee045353b62a5a25ce15e57c6d9d1020d73
Jonathan Hendriks <jonathan.hendriks@zuehlke.com>
2024-05-02 09:20:11 +0200

    Added floaterm, with keymaps

--- plugin/vim-floaterm.lua
+++ plugin/vim-floaterm.lua
@@ -1,8 +1,8 @@
 -- local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>bn', ":FloatermNew")
-vim.keymap.set('n', '<leader>bt>', ":FloatermNew")
-vim.keymap.set('n', '<leader>bb>', ":FloatermNew")
+vim.keymap.set('n', '<leader>bn', ":FloatermNew<CR>")
+vim.keymap.set('n', '<leader>bt>', ":FloatermNew<CR>")
+vim.keymap.set('n', '<leader>bb>', ":FloatermNew<CR>")
 -- vim.keymap.set('n', '<leader>sb', function()
 -- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 -- end)
