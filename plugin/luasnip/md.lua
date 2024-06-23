b87d7dec7106d698f508c0c9aa6b7db721dccc90
jonathan.hendriks <jonathan.j.hendriks@gmail.com>
2024-06-23 15:32:36 +0000

    Markdown typo

--- plugin/luasnip/md.lua
+++ plugin/luasnip/md.lua
@@ -13,16 +13,15 @@ local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 local p = require("luasnip.extras").partial
 
-function get_file_name(file)
-      local file_name = file:match("[^/]*.md$")
-      return file_name:sub(0, #file_name - 4)
-end
+-- function get_file_name(file)
+--       local file_name = file:match("[^/]*.md$")
+--       return file_name:sub(0, #file_name - 4)
+-- end
 
--- local filename = function(_,snip) 
---       local file = snip.env.TM_FILENAME 
---       return file:match("(.*).md")
--- end 
---
+local filename = function(_,snip) 
+      local file = snip.env.TM_FILENAME 
+      return file:match("(.*).md")
+end 
 
 -- ------------ SNIPPETS --------------
 ls.add_snippets("markdown", {
@@ -37,9 +36,11 @@ urls:
 ---
 
 # {}
+{}
 ]],{ 
-    t(os.date("%Y-%m-%d")),
+    f(os.date("%Y-%m-%d")),
     f(filename,{}),
+    i(0),
   })
   ),
 },
